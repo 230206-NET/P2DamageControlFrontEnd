3425151a90bdfddbd0e65b02d2ac662cef37b2d8
eperry910 <edwin501@revature.net>
2023-03-13 20:20:05 -0400

    Updated more login components

--- project2FrontEnd/src/app/services/auth.service.ts
+++ project2FrontEnd/src/app/services/auth.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5025/'; // Replace with your API URL
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<boolean> {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/LogIn`, { username: username, password: password })
+      .pipe(
+        map(result => {
+          localStorage.setItem('access_token', result.token);
+          return true;
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+  }
+
+  public get loggedIn(): boolean {
+    return (localStorage.getItem('access_token') !== null);
+  }
+
+  getAuthorizationToken() {
+    const token = localStorage.getItem('access_token');
+    return token ? token : '';
+  }
+
+  // Add the following method to include the JWT token in the headers of API requests
+  get httpOptions() {
+    const token = this.getAuthorizationToken();
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      })
+    };
+  }
+
 }
