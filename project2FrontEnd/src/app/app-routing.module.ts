8bb650fc5c1185494dcb36f3c1e32b21556dab3a
main <eperry910@yahoo.com>
2023-03-14 12:23:12 -0400

    Made progress with JWT authentication

--- project2FrontEnd/src/app/app-routing.module.ts
+++ project2FrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
+import { PostLoginComponent } from './post-login/post-login.component';
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
-  {path: 'Login', component: LoginComponent}
+  {path: 'Login', component: LoginComponent},
+  {path: 'afterLogin', component: PostLoginComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
