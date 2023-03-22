b3966dccabe84a008f42e2dfd51d83aff9777643
main <eperry910@yahoo.com>
2023-03-21 22:37:02 -0400

    Added view all tickets tests

--- project2FrontEnd/src/app/view-all-tickets/view-all-tickets.component.spec.ts
+++ project2FrontEnd/src/app/view-all-tickets/view-all-tickets.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ViewAllTicketsComponent } from './view-all-tickets.component';
 
@@ -8,18 +10,30 @@ describe('ViewAllTicketsComponent', () => {
   let component: ViewAllTicketsComponent;
   let fixture: ComponentFixture<ViewAllTicketsComponent>;
   let httpMock: HttpTestingController;
+  let mockRouter: any;
+  const mockEmployeeGuard = {
+    canActivate: () => true
+  };
 
   beforeEach(async () => {
-
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate')
+    };
     await TestBed.configureTestingModule({
       declarations: [ViewAllTicketsComponent],
-      imports: [FormsModule, HttpClientTestingModule]
+      imports: [FormsModule, HttpClientTestingModule, RouterTestingModule],
+      providers: [{ provide: Router, useValue: mockRouter },]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(ViewAllTicketsComponent);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
+
     fixture.detectChanges();
+  });
+  afterEach(() => {
+    httpMock.verify();
   });
   beforeEach(() => {
     httpMock = TestBed.inject(HttpTestingController);
@@ -34,9 +48,17 @@ describe('ViewAllTicketsComponent', () => {
     const req = httpMock.expectOne("http://localhost:5025/EmployeeViewTickets/GetPendingClaims")
 
   });
+  //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3NlcmlhbG51bWJlciI6IjMiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiNk9mQ3Jvd3MiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9naXZlbm5hbWUiOiJLYXogQnJla2tlciIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IjEiLCJleHAiOjE2Nzk0NDA0MDIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAyNSIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAyNSJ9.0AWxosew_Q0MoCyq00ZAd2-aGdiwMHs7kNCWpCM1h84"
   it('should request all tickets', () => {
     component.getPendingTickets()
     const req = httpMock.expectOne("http://localhost:5025/EmployeeViewTickets/GetPendingClaims")
     expect(req.request.method).toBe("GET")
+  })
+  it('should rule on ticket', () => {
+    component.info = 1
+    component.approveTicket(1, 1)
+    httpMock.expectOne("http://localhost:5025/EmployeeViewTickets/GetPendingClaims")
+    const req = httpMock.expectOne("http://localhost:5025/EmployeeViewTickets/UpdateTicketStatus")
+
   })
 });
--- project2FrontEnd/src/app/view-all-tickets/view-all-tickets.component.ts
+++ project2FrontEnd/src/app/view-all-tickets/view-all-tickets.component.ts
@@ -21,43 +21,44 @@ type TicketDecision = {
   styleUrls: ['./view-all-tickets.component.scss']
 })
 export class ViewAllTicketsComponent implements OnInit {
-  constructor(private http : HttpClient, private router: Router, private jwtDecoder: JwtDecodingService) {}
-  FoundTickets : Array<Tickets> = []
+  constructor(private http: HttpClient, private router: Router, private jwtDecoder: JwtDecodingService) { }
+  FoundTickets: Array<Tickets> = []
   info: number = 0
-  StatusValues : Array<string> = ["Pending", "Approved", "Denied"]
+  StatusValues: Array<string> = ["Pending", "Approved", "Denied"]
+  accessLevel: number = 0;
   //Sets Id and retrieves tickets
   ngOnInit(): void {
     const token: string | null = localStorage.getItem('jwt');
-    if(token){
+    if (token) {
       this.info = this.jwtDecoder.getId()
-    }      
+      if (typeof this.jwtDecoder.getAccessLevel() == 'number')
+        this.accessLevel = this.jwtDecoder.getAccessLevel()
+    }
     this.translatePendingTickets()
-    
-    
   }
   //Retrieves all tickets from Back-End
-  getPendingTickets() : Observable<Array<Tickets>>{
+  getPendingTickets(): Observable<Array<Tickets>> {
     return this.http.get("http://localhost:5025/EmployeeViewTickets/GetPendingClaims") as Observable<Array<Tickets>>;
-    }
-    //Translates tickets to a form that the front-end can display
-    translatePendingTickets() : void{
-      this.getPendingTickets().subscribe((data: any) => {
-        console.log(data);
-        this.FoundTickets = data;
-      });
+  }
+  //Translates tickets to a form that the front-end can display
+  translatePendingTickets(): void {
+    this.getPendingTickets().subscribe((data: any) => {
+      console.log(data);
+      this.FoundTickets = data;
+    });
   }
   //Rules on tickets based on the decision passed in
-  approveTicket(ticketId :number, decision: number) : void{
-    let finalDecision : TicketDecision = {
+  approveTicket(ticketId: number, decision: number): void {
+    let finalDecision: TicketDecision = {
       userId: this.info,
-      status : decision,
-      ticketId : ticketId,
-      AccessLevel : this.jwtDecoder.getAccessLevel(),
+      status: decision,
+      ticketId: ticketId,
+      AccessLevel: this.accessLevel,
       justification: "Filler Value"
     }
     console.log(finalDecision);
     this.http.put<AuthenticatedResponse>("http://localhost:5025/EmployeeViewTickets/UpdateTicketStatus", finalDecision, {
-      headers: new HttpHeaders({"Content-Type":"application/json"})
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
     }).subscribe({
       next: (response: AuthenticatedResponse) => {
         this.router.navigate(["/ViewClaims"])
@@ -65,5 +66,5 @@ export class ViewAllTicketsComponent implements OnInit {
       }, error: (err: HttpErrorResponse) => console.log(err)
     })
   }
-  }
+}
 
