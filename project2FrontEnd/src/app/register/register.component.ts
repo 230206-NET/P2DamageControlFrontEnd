3be3652a5030fa3734c7814e34393b08970b3903
main <eperry910@yahoo.com>
2023-03-22 13:34:42 -0400

    Completed register component tests

--- project2FrontEnd/src/app/register/register.component.ts
+++ project2FrontEnd/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {  RegisterModel } from '../_interfaces/register.model';
+import { RegisterModel } from '../_interfaces/register.model';
 import { AuthenticatedResponse } from '../_interfaces/AuthenticatedResponse';
 import { FormsModule, NgForm } from '@angular/forms';
 
@@ -12,7 +12,7 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   invalidReg: boolean | undefined;
-  info: RegisterModel = {Username:'', Password:'', Email:'', FullName:''}
+  info: RegisterModel = { Username: '', Password: '', Email: '', FullName: '' }
   constructor(private router: Router, private http: HttpClient) { }
   ngOnInit(): void {
 
@@ -24,15 +24,16 @@ export class RegisterComponent implements OnInit {
       this.http.post<AuthenticatedResponse>("http://localhost:5025/Register/Register", this.info, {
         headers: new HttpHeaders({ "Content-Type": "application/json" })
       })
-      .subscribe({
-        next: (response: AuthenticatedResponse) => {
-          console.log("The user has been successfully sent to the server");
-          this.invalidReg = false;
-          this.router.navigate(["/"]);
-        },
-        error: (err: HttpErrorResponse) => this.invalidReg = true
-      })
+        .subscribe({
+          next: (response: AuthenticatedResponse) => {
+            console.log("The user has been successfully sent to the server");
+            this.invalidReg = false;
+            this.router.navigate(["/"]);
+          },
+          error: (err: HttpErrorResponse) => this.invalidReg = true
+        })
     }
+    this.invalidReg = true
   }
 
 }
