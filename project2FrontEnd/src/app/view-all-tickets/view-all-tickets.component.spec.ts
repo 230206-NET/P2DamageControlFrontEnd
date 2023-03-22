52b10e6108b6f3e4d5cc55d5164900fc39608f77
main <eperry910@yahoo.com>
2023-03-21 21:04:06 -0400

    Made progress in all  tickets testing

--- project2FrontEnd/src/app/view-all-tickets/view-all-tickets.component.spec.ts
+++ project2FrontEnd/src/app/view-all-tickets/view-all-tickets.component.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
@@ -7,20 +7,36 @@ import { ViewAllTicketsComponent } from './view-all-tickets.component';
 describe('ViewAllTicketsComponent', () => {
   let component: ViewAllTicketsComponent;
   let fixture: ComponentFixture<ViewAllTicketsComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      declarations: [ ViewAllTicketsComponent ],
-      imports: [ FormsModule, HttpClientTestingModule ]
+      declarations: [ViewAllTicketsComponent],
+      imports: [FormsModule, HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ViewAllTicketsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  beforeEach(() => {
+    httpMock = TestBed.inject(HttpTestingController);
+    component = TestBed.createComponent(ViewAllTicketsComponent).componentInstance;
+  });
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    const req = httpMock.expectOne("http://localhost:5025/EmployeeViewTickets/GetPendingClaims")
+
   });
+  it('should request all tickets', () => {
+    component.getPendingTickets()
+    const req = httpMock.expectOne("http://localhost:5025/EmployeeViewTickets/GetPendingClaims")
+    expect(req.request.method).toBe("GET")
+  })
 });
