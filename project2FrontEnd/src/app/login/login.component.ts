ca40301e2d06f9c46bcb3d2d0d785b773f7fb625
main <eperry910@yahoo.com>
2023-03-22 13:47:46 -0400

    Implemented login component tests

--- project2FrontEnd/src/app/login/login.component.ts
+++ project2FrontEnd/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {  LoginModel } from '../_interfaces/login.model';
+import { LoginModel } from '../_interfaces/login.model';
 import { AuthenticatedResponse } from '../_interfaces/AuthenticatedResponse';
 import { NgForm } from '@angular/forms';
 
@@ -12,27 +12,28 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   invalidLogin: boolean | undefined;
-  credentials: LoginModel = {username:'', password:''};
+  credentials: LoginModel = { username: '', password: '' };
   constructor(private router: Router, private http: HttpClient) { }
   ngOnInit(): void {
-    
+
   }
   //Sets up form for login, and transmits the info to the back-end to compare info to DB
-  login = ( form: NgForm) => {
+  login = (form: NgForm) => {
     if (form.valid) {
       this.http.post<AuthenticatedResponse>("http://localhost:5025/NewLogIn/LogIn", this.credentials, {
-        headers: new HttpHeaders({ "Content-Type": "application/json"})
+        headers: new HttpHeaders({ "Content-Type": "application/json" })
       })
-      .subscribe({
-        
-        next: (response: AuthenticatedResponse) => {
-          const token = response.token;
-          localStorage.setItem("jwt", token); 
-          this.invalidLogin = false; 
-          this.router.navigate(["/afterLogin"]);
-        },
-        error: (err: HttpErrorResponse) => this.invalidLogin = true
-      })
+        .subscribe({
+
+          next: (response: AuthenticatedResponse) => {
+            const token = response.token;
+            localStorage.setItem("jwt", token);
+            this.invalidLogin = false;
+            this.router.navigate(["/afterLogin"]);
+          },
+          error: (err: HttpErrorResponse) => this.invalidLogin = true
+        })
     }
+    this.invalidLogin = true
   }
 }
